33f0dd3c58f495d5bbeb1b4e44f0910ab2a0488a
Marcusilmitico2007 <153986442+Marcusilmitico2007@users.noreply.github.com>
2024-04-22 07:32:44 +0000

    BattleShip

--- X_BattleShip/BattleShip.cpp
+++ X_BattleShip/BattleShip.cpp
@@ -26,7 +26,7 @@ class BattleShip{
         srand(time(NULL));
         mappa = BattleField( VOID );
         campo = BattleField( VOID );
-        campo.placeshipHorizontal(3);
+        campo.placeshipHorizontal(3); //piazziamo delle navi nel costruttore
         campo.placeshipVertical(4);
         campo.placeshipVertical(2);
         campo.placeshipHorizontal(5);
@@ -43,37 +43,52 @@ class BattleShip{
 
     void play(){
         //lancia 20 bombe a caso
-        for (int i = 0; i < 20; i++)
-        {
-            int x=rand()%DIM;
-            int y=rand()%DIM;
-            if(campo.get(x,y)==HIT) continue; 
-            if(campo.get(x,y)==SHIP) {
-                mappa.put(x,y,HIT);
-                campo.put(x,y,HIT);
-            } else mappa.put(x,y,MISS);
+        mappa.stampa();
+
+        while(true){
+            mappa.stampa();
+            if( !playHand()) 
+                break;
+  
         }
-        
-
-        mappa.stampa();
-
-        ask();
 
         
-        mappa.stampa();
         campo.stampa();
     }
 
-    void ask(){
-        cout<<"Inserisci le cordinate di riga e colona in cui sganciare la bomba";
+    bool playHand(){
+        cout<<"Inserisci le cordinate di riga e colona(1-"<<DIM<<") in cui sganciare la bomba";
         int x, y;
         cin>>x;
+        if(x<=0 ||x>DIM)
+           return false; else x--;
         cin>>y;
+        if(y==0|| y>DIM)
+            return false; else y--;
+            
+        
         if(campo.get(x,y)==SHIP) {
                 mappa.put(x,y,HIT);
                 campo.put(x,y,HIT);
             } else mappa.put(x,y,MISS);
+            return true;
         }
-    };
+    
+    bool GameOver(){
+        
+        for (int i = 0; i < DIM; i++) {
+            for (int j = 0; j < DIM; j++) {
+                if (campo.get(i, j) == SHIP) {
+                    return false;
+                
+            }
+        }
+        return true;
+    }
+
+
+    }
+
+};
 
 
